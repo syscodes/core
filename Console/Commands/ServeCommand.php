43b573d0bf9617cc3f98ea26ddd317cccff8216b
Alexander Campo <jalexcam@gmail.com>
2025-10-28 01:50:55 -0500

    Create ServeCommand.php

---
+++ Console/Commands/ServeCommand.php
@@ -0,0 +1,87 @@
+<?php
+
+/**
+ * Lenevor Framework
+ *
+ * LICENSE
+ *
+ * This source file is subject to the new BSD license that is bundled
+ * with this package in the file license.md.
+ * It is also available through the world-wide-web at this URL:
+ * https://lenevor.com/license
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@Lenevor.com so we can send you a copy immediately.
+ *
+ * @package     Lenevor
+ * @subpackage  Base
+ * @link        https://lenevor.com
+ * @copyright   Copyright (c) 2019 - 2025 Alexander Campo <jalexcam@gmail.com>
+ * @license     https://opensource.org/licenses/BSD-3-Clause New BSD license or see https://lenevor.com/license or see /license.md
+ */
+
+namespace Syscodes\Components\Core\Console\Commands;
+
+use Syscodes\Components\Console\Command;
+use Syscodes\Components\Support\Environment;
+use  Syscodes\Components\Console\Input\InputOption;
+use Syscodes\Components\Console\Attribute\AsCommandAttribute;
+
+/**
+ * Executable PHP server for execute the framework system.
+ */
+#[AsCommandAttribute(name: 'serve')]
+class ServeCommand extends Command
+{
+    /**
+     * The console command name.
+     * 
+     * @var string|null $name
+     */
+    protected ?string $name = 'serve';
+
+     /**
+     * The console command description.
+     * 
+     * @var string|null $description
+     */
+    protected string $description = 'Serve the application on the PHP development server';
+
+    /**
+     * Executes the current command.
+     * 
+     * @return int
+     * 
+     * @throws \LogicException
+     */
+    public function handle()
+    {
+        chdir($this->lenevor['path.base']);
+
+        $host = $this->input->getOption('host');
+
+        $port = $this->input->getOption('port');
+
+        $public = $this->lenevor['path.public'];
+
+        $this->commandline("<bg=blue;fg=white> INFO </> Server running on [http://{$host}:{$port}].");
+        $this->commandLine('       <fg=yellow;options=bold>Press Ctrl+C to stop the server</>');
+
+        $this->newline();
+
+        passthru('"'.PHP_BINARY.'"'." -S {$host}:{$port} -t \"{$public}\"");
+    }
+
+    /**
+     * Get the console command options.
+     * 
+     * @return array
+     */
+    protected function getOptions(): array
+    {
+        return [
+            ['host', null, InputOption::VALUE_OPTIONAL, 'The host address to serve the application on', Environment::get('SERVER_HOST', '127.0.0.1')],
+            ['port', null, InputOption::VALUE_OPTIONAL, 'The port to serve the application on', Environment::get('SERVER_PORT')],
+        ];
+    }
+}
